d4a02b3e8cc4f3dccdf3cfcd0e469731fb470632
michalbis <michal.stanislaw.bis@outlook.com>
2019-07-29 00:36:41 +0200

    first commit

---
+++ aisframe/build.sbt
@@ -0,0 +1,11 @@
+name := "AIS_AWS_aisframe"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.4.0",
+  "org.apache.spark" %% "spark-sql" % "2.4.0"
+)
---
+++ aisframe/src/main/scala/aisframe.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object AISframe
+{
+  def checkimo(imo: String): Boolean =
+  {
+    if(imo.length!=7)
+      return false;
+    val x = imo.map(_.toInt-'0'.toInt);
+    if (((7 to 2 by -1 zip x.dropRight(1)).map{case(a,b)=>a*b}.sum-x(6))%10==0)
+      return true;
+    return false
+  }
+  def main(args: Array[String])
+  {
+    val hdfsprefix = "hdfs://ip-10-0-1-92.eu-west-1.compute.internal:8020/"
+    val tfiles = hdfsprefix + args(0)
+    val outputfile = hdfsprefix + args(1)
+
+    val conf = new SparkConf()
+    conf.setAppName("AIS-frame")
+    conf.setMaster("yarn-client")
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile(tfiles)
+      .map(_.split(";"))
+      //.filter(x=> x(0)!="mmsi")
+
+    val imos = data.map(x => x(2)).distinct().filter(x=>checkimo(x));
+    imos.saveAsTextFile(outputfile);
+
+    sc.stop()
+  }
+}
